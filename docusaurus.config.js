a1db5a1acc2e44b68a77cde8f322022275d1b15a
greg-norris <greg.norris@ghielectronics.com>
2025-02-04 13:02:34 -0500

    Add ghielectronics-social-card and update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,7 +82,7 @@ const config = {
       },
 
       // Replace with your project's social card
-      //image: 'img/docusaurus-social-card.jpg',
+      image: 'img/ghielectronics-social-card.jpg',
       navbar: {
         title: 'by GHI Electronics',
         logo: {
