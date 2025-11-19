c0be60877e109189707c703e86e869640357b2c2
Dat Tran <dat.tran@ghielectronics.com>
2025-11-19 15:27:59 -0500

    Fixed arduino SPI sending array

--- static/code/sample/standalone/gdl-sptree-b.ino
+++ static/code/sample/standalone/gdl-sptree-b.ino
@@ -20,27 +20,32 @@ void SetLed(uint32_t index, uint32_t color, uint32_t level) {
 }
 
 void ShowLed() {
-    SPI.beginTransaction(tree_settings);
-    SPI.transfer(led_data,BUFF_LEN);
-    SPI.endTransaction();
+  SPI.beginTransaction(tree_settings);
+  for (size_t i = 0; i < BUFF_LEN; i++) {
+    SPI.transfer(led_data[i]);
+  }
+  //SPI.transfer(led_data,nullptr,BUFF_LEN);
+  SPI.endTransaction();   
 }
 
 void setup() {
   // initialize digital pin as an output.
   pinMode(PB8, OUTPUT);
+  digitalWrite(PB8, HIGH);
   //init SPI pins
   // SCK, MISO, MOSI, CS
   SPI.begin();//PB3, PB4, PB5, PC6);
   SPI.setMISO(PB4);
   SPI.setMOSI(PB5);
   SPI.setSCLK(PB3);
+  SPI.setBitOrder(LSBFIRST);
   memset(led_data,0,BUFF_LEN);
   // first 4 bytes need to be 0
   for (int i = 0; i<4; i++)
       led_data[i] = 0;
 
   // All led off
-  for (int i = 1 ; i< 10; i++)
+  for (int i = 1 ; i<= NUM_LED; i++)
       SetLed(i, 0, 0);
 
   // last 4 bytes need to be 255
@@ -52,18 +57,19 @@ void setup() {
 
 void loop() {
 
-  for(int led = 1; led<=10;led++){
+  for(int led = 1; led<=NUM_LED;led++){
     int32_t r = random(50);
     int32_t g = random(50);
     int32_t b = random(50);
-    //int_32_t l = Rnd(31)
+    int32_t l = random(31);
 
     SetLed(led, r<<16|g<<8|b, 10);
   }
   ShowLed();
+  
 
   digitalWrite(PB8, HIGH);
-  delay(500);
+  delay(50);
   digitalWrite(PB8, LOW);
-  delay(500);
+  delay(50);
 }
