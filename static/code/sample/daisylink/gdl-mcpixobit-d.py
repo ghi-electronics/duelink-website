9deed250e1e61f65c8bf15bbec95875ebf17c9ea
Dat Tran <dat.tran@ghielectronics.com>
2025-11-21 09:15:03 -0500

    add python pixobit

--- static/code/sample/daisylink/gdl-mcpixobit-d.py
+++ static/code/sample/daisylink/gdl-mcpixobit-d.py
@@ -1,18 +1,38 @@
-from DUELink.DUELinkController import DUELinkController
+# This sample draws circles on the screen and plays a sweep sound when a touch is detected on pin 0, 1, or 2
 import time
+from DUELink.DUELinkController import DUELinkController
+
 availablePort = DUELinkController.GetConnectionPort()
 duelink = DUELinkController(availablePort)
 
+def IsTouch(pin):
+    if pin < 0 or pin > 2:
+        return False
+
+    cmd = f"istouch({pin})"
+    ret = int(duelink.Engine.ExecuteCommand(cmd))
+
+    return ret == 1
+
 x = 50
 d = 10
-width = int(float(duelink.Engine.Run("getw()")))
-height = int(float(duelink.Engine.Run("geth()")))
+width = int(duelink.Engine.ExecuteCommand("getw()"))
+height = int(duelink.Engine.ExecuteCommand("geth()"))
 
 while True:
-    duelink.Graphics.Circle(1, x, height//2, 10)
+    duelink.Graphics.Circle(1, x, height // 2, 10)
     duelink.Graphics.Show()
     x = x + d
-    if (x > width or x < 0):
+
+    if x > width or x < 0:
         d = d * -1
         duelink.Graphics.Clear(0)
-    time.sleep(0.1)
+
+    for i in range(3):
+        if IsTouch(i):
+            duelink.Sound.Sweep(4, 1000, 2000, 50, 255, 500)
+            break
+
+    time.sleep(0.001)  # Thread.Sleep(1) in C# is 1 ms
+
+
