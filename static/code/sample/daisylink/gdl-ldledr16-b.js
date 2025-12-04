76407067372f46c8311afce8e176a671afbee1be
Dat Tran <dat.tran@ghielectronics.com>
2025-12-04 13:01:03 -0500

    led r16: add daisylink

--- static/code/sample/daisylink/gdl-ldledr16-b.js
+++ static/code/sample/daisylink/gdl-ldledr16-b.js
@@ -1,2 +1,33 @@
-const { SerialUSB } = require("dlserialusb");
-const { DUELinkController } = require("duelink");
+// In this sample:
+// Scan from led D1 to led D16
+
+import pkg_serialusb from 'dlserialusb';
+const {SerialUSB} = pkg_serialusb
+
+import pkg_duelink from 'duelink';
+const {DUELinkController} = pkg_duelink
+
+let duelink = new DUELinkController(new SerialUSB());
+await duelink.Connect();
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function SetLed(index, value) {
+  const val = value ? 1 : 0;
+  await duelink.Engine.ExecuteCommand(`SetLed(${index},${val})`);
+}
+
+async function main() {
+  let count = 0;
+
+  while (true) {
+    await SetLed((count % 16) + 1, Math.floor(count / 16) % 2 === 0);
+    count++;
+    await sleep(100); // 100 ms
+  }
+}
+
+main();
+
--- static/code/sample/daisylink/gdl-ldledr16-b.py
+++ static/code/sample/daisylink/gdl-ldledr16-b.py
@@ -1,4 +1,18 @@
-from DUELink.DUELinkController import DUELinkController
+# In this sample:
+# Scan from led D1 to led D16
+
 import time
+from DUELink.DUELinkController import DUELinkController
+
 availablePort = DUELinkController.GetConnectionPort()
 duelink = DUELinkController(availablePort)
+
+def SetLed(index, value):
+    val = 1 if value else 0
+    duelink.Engine.ExecuteCommand(f"SetLed({index},{val})")
+
+count = 0
+while True:
+    SetLed((count % 16) + 1, (count // 16) % 2 == 0)
+    count += 1
+    time.sleep(0.1)  # 100 ms
