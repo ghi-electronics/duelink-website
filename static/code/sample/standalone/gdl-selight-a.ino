20791e4a50f5b1084df035d697c20118b4819bb3
Dat Tran <dat.tran@ghielectronics.com>
2025-12-04 12:09:17 -0500

    add standalone

---
+++ static/code/sample/standalone/gdl-selight-a.ino
@@ -0,0 +1,41 @@
+// In this sample:
+// Pull light value every second
+// When light below 25, turn the statled on
+// When light higher 25, turn the statled off
+
+#define PIN_LIGHT   getpin(1)  // CLK (A) 
+#define PIN_STATLED getpin(0) // Statled pin
+
+const uint8_t pin_map[] = {PB8, PA0, PA1, PA4, PA5, PA6, PA7, PA8, PB1, PB0, PC15, PC6, PB3, PB4, PB5,PB6,PB7,PB2,PB9,PA15,PA14,PA9,PA10,PA13,PC14};
+
+const int getpin(int pin) {
+  // Input mapped DUELink pins: P1, P2, P3....
+  // Return the actual pin (PA0, PA1.....)
+  if (pin < 0 || pin > sizeof(pin_map) )
+    return -1;
+
+  return pin_map[pin];
+}
+
+int light() {
+  int sensorValue = analogRead(PIN_LIGHT); // return 0 to 4095
+  int scaled = map(sensorValue, 0, 4095, 0, 100);
+
+  return scaled;
+}
+
+void setup() {  
+  pinMode(PIN_STATLED, OUTPUT);
+  analogReadResolution(12); // set to 12 bit
+
+}
+
+void loop() { 
+  if (light() < 25)
+    digitalWrite(PIN_STATLED, 1);
+  else
+    digitalWrite(PIN_STATLED, 0);
+  
+
+  delay(1000);  
+}
