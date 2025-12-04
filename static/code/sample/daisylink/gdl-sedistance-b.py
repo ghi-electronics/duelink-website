ac61733aab5cb39161fc667e777b7de673ea8751
Dat Tran <dat.tran@ghielectronics.com>
2025-12-04 08:56:17 -0500

    add distance daisylink

--- static/code/sample/daisylink/gdl-sedistance-b.py
+++ static/code/sample/daisylink/gdl-sedistance-b.py
@@ -1,4 +1,17 @@
-from DUELink.DUELinkController import DUELinkController
+# In this sample:
+# Update distance in cm every second
+
 import time
+from DUELink.DUELinkController import DUELinkController
+
 availablePort = DUELinkController.GetConnectionPort()
 duelink = DUELinkController(availablePort)
+
+def GetDistance():
+    ret = duelink.Engine.ExecuteCommand("Distance()")
+    return ret
+
+while True:
+    distance = GetDistance()
+    print(f"Distance: {distance} cm")
+    time.sleep(1)
