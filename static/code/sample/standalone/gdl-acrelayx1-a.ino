3c83c4c861f7cc269b7283d7d5301c44987db56b
Dat Tran <dat.tran@ghielectronics.com>
2026-01-09 13:13:39 -0500

    add relay x1

---
+++ static/code/sample/standalone/gdl-acrelayx1-a.ino
@@ -0,0 +1,35 @@
+// In this sample:
+// Continuously switch relay on/off every 2 seconds
+
+#define SW_SERIAL_DBG 0 // optional: this will use PA2, PA3 as software serial for debug runtime value
+
+#if SW_SERIAL_DBG
+#include <SoftwareSerial.h> 
+SoftwareSerial softwareSerial(PA2, PA3);
+#endif
+const uint8_t pin_map[] = {PB8, PA0, PA1, PA4, PA5, PA6, PA7, PA8, PB1, PB0, PC15, PC6, PB3, PB4, PB5,PB6,PB7,PB2,PB9,PA15,PA14,PA9,PA10,PA13,PC14};
+
+const int getpin(int pin) {
+  // Input mapped DUELink pin: P1, P2, P3
+  // Return STM32 pin:PA0, PA1, PB0....
+  return pin_map[pin];
+}
+
+void setup() {
+#if SW_SERIAL_DBG  
+  softwareSerial.begin(9600);
+#endif
+   pinMode(getpin(19), OUTPUT);
+}
+
+// Set relay function
+void Set(int r, int v) {   
+    digitalWrite(getpin(19), v);
+}
+
+void loop() {  
+  Set(1);
+  delay(2000);
+  Set(0);
+  delay(2000);
+}
--- static/code/sample/tethered/gdl-acrelayx1-a.js
+++ static/code/sample/tethered/gdl-acrelayx1-a.js
@@ -1,2 +1,34 @@
-const { SerialUSB } = require("dlserialusb");
-const { DUELinkController } = require("duelink");
+// In this sample:
+// Continuously switch relay on/off every 2 seconds
+
+import pkg_serialusb from 'dlserialusb';
+const {SerialUSB} = pkg_serialusb
+
+import pkg_duelink from 'duelink';
+const {DUELinkController} = pkg_duelink
+
+let duelink = new DUELinkController(new SerialUSB());
+await duelink.Connect();
+
+// Sleep helper (ms)
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Methods
+async function Set(value) {
+    const v = value ? 1 : 0;
+    await duelink.Engine.ExecuteCommand(`Set(${v})`);
+}
+
+async function main() {
+    while (true) {
+        await sleep(2000);
+        await Set(true);
+
+        await sleep(2000);
+        await Set(false);
+    }
+}
+
+main();
