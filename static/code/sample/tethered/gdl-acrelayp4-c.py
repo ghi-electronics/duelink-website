a33375bfa49cde5e12253c9046c728ae1d541796
Dat Tran <dat.tran@ghielectronics.com>
2026-01-09 12:42:05 -0500

    add relay p4 (teh)

--- static/code/sample/tethered/gdl-acrelayp4-c.py
+++ static/code/sample/tethered/gdl-acrelayp4-c.py
@@ -1,4 +1,20 @@
-from DUELink.DUELinkController import DUELinkController
+# In this sample:
+# Continuously switch relay on/off every 2 seconds
+
 import time
+from DUELink.DUELinkController import DUELinkController
+
 availablePort = DUELinkController.GetConnectionPort()
 duelink = DUELinkController(availablePort)
+
+# method
+def Set(relay, value):
+    v = 1 if value else 0
+    duelink.Engine.ExecuteCommand(f"Set({relay},{v})")
+
+while True:
+    time.sleep(2)
+    Set(2, True)
+
+    time.sleep(2)
+    Set(2, False)
