24ec5aa2d0a4b4767d41e0910325e032606b917e
Dat Tran <dat.tran@ghielectronics.com>
2026-01-09 11:47:16 -0500

    add samples fan (teh.)

--- static/code/sample/tethered/gdl-acfan-b.py
+++ static/code/sample/tethered/gdl-acfan-b.py
@@ -1,4 +1,24 @@
-from DUELink.DUELinkController import DUELinkController
+# In this sample:
+# Continuously turn the fan forward at 50% speed, then backward at 50% speed, with a 2-second delay between each direction change
+
 import time
+from DUELink.DUELinkController import DUELinkController
+
 availablePort = DUELinkController.GetConnectionPort()
 duelink = DUELinkController(availablePort)
+
+# method
+def SetSpeed(speed):
+    v = speed
+    if v > 100:
+        v = 100
+    if v < -100:
+        v = -100
+    duelink.Engine.ExecuteCommand(f"Fan({v})")
+
+while True:
+    time.sleep(2)  # 2000 milliseconds
+    SetSpeed(50)
+
+    time.sleep(2)
+    SetSpeed(-50)
