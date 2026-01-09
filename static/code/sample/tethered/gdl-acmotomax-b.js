249c9e582ae3bc0ace9c460632e5e0caa63f7802
Dat Tran <dat.tran@ghielectronics.com>
2026-01-09 15:03:11 -0500

    add motomax (teth.)

--- static/code/sample/tethered/gdl-acmotomax-b.js
+++ static/code/sample/tethered/gdl-acmotomax-b.js
@@ -1,2 +1,32 @@
-const { SerialUSB } = require("dlserialusb");
-const { DUELinkController } = require("duelink");
+// In this sample:
+// Continuously turn the motor forward at 50% speed, then backward at 50% speed, with a 2-second delay between each direction change
+
+import pkg_serialusb from 'dlserialusb';
+const {SerialUSB} = pkg_serialusb
+
+import pkg_duelink from 'duelink';
+const {DUELinkController} = pkg_duelink
+
+let duelink = new DUELinkController(new SerialUSB());
+await duelink.Connect();
+
+// Sleep helper (ms)
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Methods
+async function Set(speed) {
+    await duelink.Engine.ExecuteCommand(`Set(${speed})`);
+}
+
+async function main() {
+    while (true) {
+        await Set(50);       // forward
+        await sleep(2000);   // 2000 ms
+        await Set(-50);      // backward
+        await sleep(2000);
+    }
+}
+
+main();
--- static/code/sample/tethered/gdl-acmotomax-b.py
+++ static/code/sample/tethered/gdl-acmotomax-b.py
@@ -1,4 +1,18 @@
-from DUELink.DUELinkController import DUELinkController
+# In this sample:
+# Continuously turn the motor forward at 50% speed, then backward at 50% speed, with a 2-second delay between each direction change
+
 import time
+from DUELink.DUELinkController import DUELinkController
+
 availablePort = DUELinkController.GetConnectionPort()
 duelink = DUELinkController(availablePort)
+
+# method
+def Set(speed):
+    duelink.Engine.ExecuteCommand(f"Set({speed})")
+
+while True:
+    Set(50)      # forward
+    time.sleep(2)  # 2000 ms -> 2 seconds
+    Set(-50)     # backward
+    time.sleep(2)
