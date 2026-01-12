26045b20bcd29db19d0b0b5fb25eedd4de0a7d06
Dat Tran <dat.tran@ghielectronics.com>
2026-01-12 10:33:41 -0500

    add stepulse teth

--- static/code/sample/tethered/gdl-acstepp-a.cs
+++ static/code/sample/tethered/gdl-acstepp-a.cs
@@ -1,1 +1,39 @@
-// Work in Progress
+// In this sample:
+// Tested on Bi-Polar Stepper Motor Driver-2M542
+// Continuously move axes X, Y, and Z forward and backward for 400 steps, with a 10 ms delay per step.
+
+using GHIElectronics.DUELink;
+
+var availablePort = DUELinkController.GetConnectionPort();
+var duelink = new DUELinkController(availablePort);
+
+void StepX(int direction, int stepnum) {
+    duelink.Engine.ExecuteCommand($"StepX({direction},{stepnum})");
+    
+    //Blocking mode
+    Thread.Sleep(stepnum);
+}
+void StepY(int direction, int stepnum) {
+    duelink.Engine.ExecuteCommand($"StepY({direction},{stepnum})");
+
+    //Blocking mode
+    Thread.Sleep(stepnum);
+}
+
+void StepZ(int direction, int stepnum) {
+    duelink.Engine.ExecuteCommand($"StepZ({direction},{stepnum})");
+
+    //Blocking mode
+    Thread.Sleep(stepnum);
+}
+
+while (true) {
+    StepX(1, 400); // forward
+    StepY(1, 400); // forward
+    StepZ(1, 400); // forward
+    Thread.Sleep(1000);
+    StepX(0, 400); // forward
+    StepY(0, 400); // forward
+    StepZ(0, 400); // forward
+    Thread.Sleep(1000);
+}
--- static/code/sample/tethered/gdl-acstepp-a.js
+++ static/code/sample/tethered/gdl-acstepp-a.js
@@ -1,1 +1,53 @@
-// Work in Progress
+// In this sample:
+// Tested on Bi-Polar Stepper Motor Driver-2M542
+// Continuously move axes X, Y, and Z forward and backward for 400 steps, with a 10 ms delay per step.
+
+import pkg_serialusb from 'dlserialusb';
+const {SerialUSB} = pkg_serialusb
+
+import pkg_duelink from 'duelink';
+const {DUELinkController} = pkg_duelink
+
+let duelink = new DUELinkController(new SerialUSB());
+await duelink.Connect();
+
+// Sleep helper (ms)
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Methods
+async function StepX(direction, stepnum) {
+    await duelink.Engine.ExecuteCommand(`StepX(${direction},${stepnum})`);
+
+    // Blocking mode
+    await sleep(stepnum);
+}
+
+async function StepY(direction, stepnum) {
+    await duelink.Engine.ExecuteCommand(`StepY(${direction},${stepnum})`);
+
+    // Blocking mode
+    await sleep(stepnum);
+}
+
+async function StepZ(direction, stepnum) {
+    await duelink.Engine.ExecuteCommand(`StepZ(${direction},${stepnum})`);
+
+    // Blocking mode
+    await sleep(stepnum);
+}
+
+(async function main() {
+    while (true) {
+        await StepX(1, 400); // forward
+        await StepY(1, 400); // forward
+        await StepZ(1, 400); // forward
+        await sleep(1000);
+
+        await StepX(0, 400); // backward
+        await StepY(0, 400); // backward
+        await StepZ(0, 400); // backward
+        await sleep(1000);
+    }
+})();
