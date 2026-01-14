649ecf74e16805852948e90aa9cba104c993bee0
Dat Tran <dat.tran@ghielectronics.com>
2026-01-14 13:57:37 -0500

    add dial sample

--- static/code/sample/tethered/gdl-hidial-a.py
+++ static/code/sample/tethered/gdl-hidial-a.py
@@ -1,4 +1,16 @@
-from DUELink.DUELinkController import DUELinkController
+# In this sample:
+# Update Dial value every second
+
 import time
+from DUELink.DUELinkController import DUELinkController
+
 availablePort = DUELinkController.GetConnectionPort()
 duelink = DUELinkController(availablePort)
+
+# method
+def Dial():
+    return duelink.Engine.ExecuteCommand("Dial()")
+
+while True:
+    print(f"Value: {Dial()}")
+    time.sleep(1)  # Sleep for 1 second
