71a2c57880601511e1af630397fbc986ffd33799
Dat Tran <dat.tran@ghielectronics.com>
2026-01-15 12:35:30 -0500

    add thermo sample (teth.)

--- static/code/sample/tethered/gdl-sethermo-a.cs
+++ static/code/sample/tethered/gdl-sethermo-a.cs
@@ -1,1 +1,18 @@
-// Work in Progress
+// In this sample:
+// Read temperature every 1.5 second
+// ±2°C Accuracy
+
+using System.Data.SqlTypes;
+using GHIElectronics.DUELink;
+
+var availablePort = DUELinkController.GetConnectionPort();
+var duelink = new DUELinkController(availablePort);
+
+float GetTemperature() {
+    return duelink.Engine.ExecuteCommand("ReadTemp()");
+}
+
+while (true) {   
+    Console.WriteLine($"Temperature: {GetTemperature().ToString()} ");
+    Thread.Sleep(1500);
+}
--- static/code/sample/tethered/gdl-sethermo-a.py
+++ static/code/sample/tethered/gdl-sethermo-a.py
@@ -1,1 +1,19 @@
-# Work in Progress
+# In this sample:
+# Read temperature every 1.5 second
+# ±2°C Accuracy
+
+import time
+from datetime import datetime
+from DUELink.DUELinkController import DUELinkController
+
+availablePort = DUELinkController.GetConnectionPort()
+duelink = DUELinkController(availablePort)
+
+# method
+def GetTemperature():
+    return float(duelink.Engine.ExecuteCommand("ReadTemp()"))
+
+while True:
+    print(f"Temperature: {GetTemperature()} ")    
+
+    time.sleep(1.5)  # 1500 ms
